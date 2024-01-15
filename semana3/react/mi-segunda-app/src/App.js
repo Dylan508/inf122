4cbfebebd1661ae9829e1e9b20c3da5d649c4420
Dylan508 <155546887+Dylan508@users.noreply.github.com>
2024-01-15 14:31:15 +0000

    cambio8000

--- semana3/react/mi-segunda-app/src/App.js
+++ semana3/react/mi-segunda-app/src/App.js
@@ -1,20 +1,21 @@
 import './App.css';
 import Boton from './components/Boton';
 import Contador from './components/Contador';
-import { useState } from 'react';
+//import { useState } from 'react';
 
 function App() {
-  const [nroClicks, setNumClicks] = useState(0);
+  //const [nroClicks, setNumClicks] = useState(0);
   const click = () => {
-    setNumClicks(nroClicks + 1);
+    
+    console.log("click")
   }
   const reiniciar = () => {
-    setNumClicks(0);
+    console.log("reiniciar")
   }
   return (
     <div className="App">
       <div className="contenedor-principal">
-        <Contador nroClicks={nroClicks} />
+        <Contador nroClicks={0} />
         <div className='contendedor-botones'>
           <Boton texto="Click" esBotonClick={true}
             funcionClick={click} />
