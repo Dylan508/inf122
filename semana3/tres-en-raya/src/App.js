b9698df5b0a9ab474da60bf741cd3b7816036054
Dylan508 <155546887+Dylan508@users.noreply.github.com>
2024-01-17 14:46:27 +0000

    caaambioooooo

--- semana3/tres-en-raya/src/App.js
+++ semana3/tres-en-raya/src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
-import TableroD from './components/TableroD.js'
-import Tablero from './components/Tablero.js'
+/*import TableroD from './components/TableroD.js'*/
+/*import Tablero from './components/Tablero.js'*/
+import Juego from './components/Juego';
 
 
 function App() {
   return (
     <div className="App">
-      <Tablero/>
+      <Juego/>
     </div>
   );
 }
-
+// desaparecer el historial
+//
 export default App;
