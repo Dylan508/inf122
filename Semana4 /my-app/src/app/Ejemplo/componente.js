8ddb3f49c46efd90c8791949e56277aa6d685aad
Dylan508 <155546887+Dylan508@users.noreply.github.com>
2024-01-23 19:47:03 +0000

    cambio

---
+++ Semana4 /my-app/src/app/Ejemplo/componente.js
@@ -0,0 +1,5 @@
+export default function ejemplo() {
+    return (
+      <p> componenete</p>
+    );
+  }
---
+++ Semana4 /my-app/src/app/page.js
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    // <main className={styles.main}>
+    //   <div className={styles.description}>
+    //   <header>MI PRIMERA PAGINA EN NEXT</header>
+    //   </div>
+    // </main>
+    <h1>Mi primera App de Next.js</h1>
+  );
+}
